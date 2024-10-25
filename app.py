02a701b1286530485a6f827704760fae8825fb8f
KleivZ <kleivz.a@gmail.com>
2024-10-25 17:01:29 +0200

    La til kontakt.html, og om_meg.html sidene, og gjorde det mulig å navigere ditt ved å navigere siden.

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/om_meg")
+def om_meg():
+    return render_template("om_meg.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
