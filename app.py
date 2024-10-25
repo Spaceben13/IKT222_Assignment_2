62b8003dcfcda07e70670fbb17f69da000571e86
KleivZ <kleivz.a@gmail.com>
2024-10-25 17:25:02 +0200

    skapt et uniformt design, og lagt til base.html.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,9 +11,14 @@ def index():
 def om_meg():
     return render_template("om_meg.html")
 
-@app.route("/kontakt")
+@app.route('/kontakt', methods=['GET', 'POST'])
 def kontakt():
-    return render_template("kontakt.html")
+    if request.method == 'POST':
+        # Her kan du legge til kode for å håndtere innsending av kontaktskjemaet.
+        # For eksempel, lagre dataene i en database eller sende en e-post.
+        return "Takk for meldingen din!"
+    else:
+        return render_template('kontakt.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
